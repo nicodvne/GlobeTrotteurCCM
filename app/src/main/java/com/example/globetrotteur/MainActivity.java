b0fce33f13ca3311f57047bbed552f72a350bdd8
EstebanRestoy <esteban.restoy@etu.uca.fr>
2022-06-04 16:48:49 +0200

    [ADD] coordonées sur l'image

--- app/src/main/java/com/example/globetrotteur/MainActivity.java
+++ app/src/main/java/com/example/globetrotteur/MainActivity.java
@@ -2,55 +2,60 @@ package com.example.globetrotteur;
 
 
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Looper;
-import android.provider.Settings;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.github.dhaval2404.imagepicker.ImagePicker;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
+
+import java.io.File;
 
 
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
-public class MainActivity extends AppCompatActivity {
-
-    FusedLocationProviderClient mFusedLocationClient;
+    private LocationManager locationManager;
     private final String url = "http://10.0.2.2:8080/TestAndroid/UploadServlet";
     private String lon = "";
     private String lat = "";
     private Button button_choose_picture;
     public static final int PHOTO_ACTIVITY_RETURN_CODE = 42;
-    int PERMISSION_ID = 44;
-
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
 
+        // Initialize the location fields
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+            lon = ("Location not available");
+            lat = ("Location not available");
+        }
     }
 
     public void onClickChoosePicture(View view) {
         ImagePicker.with(this)
                 .compress(1024)            //Final image size will be less than 1 MB(Optional).
-                .maxResultSize(1080, 1080)    //Final image resolution will be less than 1080 x 1080(Optional)
+                .maxResultSize(1080, 1080)
                 .start(PHOTO_ACTIVITY_RETURN_CODE);
 
     }
@@ -59,12 +64,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         try {
             super.onActivityResult(requestCode, resultCode, data);
-
             if (requestCode == PHOTO_ACTIVITY_RETURN_CODE  && resultCode  == RESULT_OK) {
-                getLastLocation();
-                Toast.makeText(MainActivity.this, data.getData().toString(), Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this,lon + " " +lat, Toast.LENGTH_SHORT).show();
-
+                //Toast.makeText(MainActivity.this, data.getData().toString(), Toast.LENGTH_SHORT).show();
+                File image = new File(((Uri)data.getData()).getPath());
+                String newFile = renameFile(image, lon +","+ lat + ".png");
+                Toast.makeText(MainActivity.this,lon + " : " + lat + " " + newFile, Toast.LENGTH_SHORT).show();
             }
         } catch (Exception ex) {
             Toast.makeText(MainActivity.this, ex.toString(),
@@ -73,105 +77,49 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-    @SuppressLint("MissingPermission")
-    private void getLastLocation() {
-        // check if permissions are given
-        if (checkPermissions()) {
-
-            // check if location is enabled
-            if (isLocationEnabled()) {
-
-                // getting last
-                // location from
-                // FusedLocationClient
-                // object
-                mFusedLocationClient.getLastLocation().addOnCompleteListener(task -> {
-                    Location location = task.getResult();
-                    if (location == null) {
-                        requestNewLocationData();
-                    } else {
-                        lon = String.valueOf(location.getLatitude());
-                        lat = String.valueOf(location.getLongitude());
-                    }
-                });
-            } else {
-                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        } else {
-            // if permissions aren't available,
-            // request for permissions
-            requestPermissions();
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private void requestNewLocationData() {
-
-        // Initializing LocationRequest
-        // object with appropriate methods
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(5);
-        mLocationRequest.setFastestInterval(0);
-        mLocationRequest.setNumUpdates(1);
-
-        // setting LocationRequest
-        // on FusedLocationClient
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
-    }
-
-    private LocationCallback mLocationCallback = new LocationCallback() {
-
-        @Override
-        public void onLocationResult(LocationResult locationResult) {
-            Location mLastLocation = locationResult.getLastLocation();
-            lon = String.valueOf(mLastLocation.getLatitude());
-            lat = String.valueOf(mLastLocation.getLongitude());
-        }
-    };
-
-    // method to check for permissions
-    private boolean checkPermissions() {
-        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED;
-
-    }
-
-    // method to request for permissions
-    private void requestPermissions() {
-        ActivityCompat.requestPermissions(this, new String[]{
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
-    }
-
-    // method to check
-    // if location is enabled
-    private boolean isLocationEnabled() {
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-    }
-
-    // If everything is alright then
-    @Override
-    public void
-    onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == PERMISSION_ID) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getLastLocation();
+    private String renameFile(File file, String name) {
+        File dir = Environment.getExternalStorageDirectory();
+        if(dir.exists()){
+            File imageNewName = new File(file.getParent(), name);
+            if(file.exists()){
+                if(file.renameTo(imageNewName))
+                    return imageNewName.getPath();
             }
         }
+        return "NULL";
+
     }
 
     @Override
-    public void onResume() {
+    protected void onResume() {
         super.onResume();
-        if (checkPermissions()) {
-            getLastLocation();
-        }
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
     }
+
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+       lat = String.valueOf((Double) (location.getLatitude()));
+       lon = String.valueOf((Double) (location.getLongitude()));
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+
 }
